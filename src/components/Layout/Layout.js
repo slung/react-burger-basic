74a7e26f50c61cd70b04608d717432bd7fa45506
Silviu Lung <slung@maporama.com>
2018-08-23 17:12:12 +0300

    lecture 139

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import Wrap from '../../hoc/Wrap';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Wrap>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Wrap>
-);
+class Layout extends Component { 
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => { 
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => { 
+        this.setState((prevState) => { 
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    render() { 
+        return (
+            <Wrap>
+                <div>Toolbar, SideDrawer, Backdrop</div>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Wrap>
+        );
+    }
+}
+
+export default Layout;
